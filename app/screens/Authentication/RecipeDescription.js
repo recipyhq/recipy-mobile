a1ed0236d49e88cfcdfcca9fa32b8030b2cc24a0
Thomas Fossaert <thomas.fossaert@epitech.eu>
2018-11-30 17:59:57 +0100

    [FEAT][RecipeDescription] Adding style

--- app/screens/Authentication/RecipeDescription.js
+++ app/screens/Authentication/RecipeDescription.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import {
-  Text, SectionList, ScrollView, Image,
+  Text, SectionList, ScrollView, Image, View,
 } from 'react-native';
 import EStyleSheet from 'react-native-extended-stylesheet';
 import { PropTypes } from 'prop-types';
@@ -20,9 +20,37 @@ const style = EStyleSheet.create({
     textAlign: 'center',
   },
 
+  sectionTitleBorder: {
+    fontSize: 20,
+    paddingTop: 10,
+    textAlign: 'center',
+    borderRadius: 2,
+    borderTopWidth: 0.5,
+    borderColor: '#d6d7da',
+  },
+
+  sectionStep: {
+    fontSize: 15,
+    paddingTop: 10,
+    color: 'orange',
+  },
+
   // Section Text
   sectionText: {
     paddingLeft: 20,
+  },
+
+  recipeTitle: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    textAlign: 'center',
+  },
+
+  information: {
+    fontSize: 30,
+    color: 'blue',
+    padding: 10,
+    borderRightWidth: 0.5,
   },
 });
 
@@ -75,10 +103,11 @@ class ForgottenPassword extends Component {
           color={colors.primaryGrey}
           fontSize={20}
         />
-        <Text
-          style={{ fontWeight: 'bold', fontSize: 20, textAlign: 'center' }}
-        >
-            Welsh de Papa Fossaert
+        <Text style={style.recipeTitle}>
+          Welsh de Papa Fossaert
+        </Text>
+        <Text>
+          5/5
         </Text>
         <Image
           style={{
@@ -86,9 +115,17 @@ class ForgottenPassword extends Component {
           }}
           source={{ uri: 'http://leflobart-leportel.fr/wp-content/uploads/2016/08/welsh.png' }}
         />
+        <View style={{ flexDirection: 'row' }}>
+          <Text style={style.information}>
+            25 min
+          </Text>
+          <Text style={style.information}>
+            Facile
+          </Text>
+        </View>
         <SectionList
           sections={[
-            { title: 'Ingrédients (4 personnes)', data: ['-1000g de cheddar', '-4 grosses tranche de pains', '-4 tranches de jambon', '-2 cuillères de moutardes', '-1 verre de bière', '-Poivre'] },
+            { title: 'Ingrédients', data: ['-1000g de cheddar', '-4 grosses tranche de pains', '-4 tranches de jambon', '-2 cuillères de moutardes', '-1 verre de bière', '-Poivre'] },
             { title: 'Matériel', data: ['-Casserole', '-Four', '-Cuiellere en bois/plastique', '-Grille pain'] },
           ]}
           renderItem={({ item }) => <Text style={style.sectionText}>{item}</Text>}
@@ -97,13 +134,22 @@ class ForgottenPassword extends Component {
             }
           keyExtractor={(item, index) => index}
         />
+        <Text style={style.sectionTitle}>
+          Préparation
+        </Text>
         <SectionList
           sections={[
-            { title: 'Préparation', data: ['-Dans une grille pain ou au four, faire griller les tranches de pains, les poser dans un plat avec la tranche de jambon par dessus', '-Faire chauffer la bière dans une casserole', '-Ajouter les cheddar au fur et à mesure et mélanger sans cesse. Ajouter la moutarde et le poivre', '-Mélanger jusqua obtenir un textur homogène', '-Faire couler le cheddar fondu sur chaque tranche de pain', '-Enfourner 20 minutes à 180 degrès en chaleur tournante et déguster !', 'test', 'test', 'test', 'test', 'test', 'test'] },
+            { title: 'Etape 1:', data: ['-Dans une grille pain ou au four, faire griller les tranches de pains, les poser dans un plat avec la tranche de jambon par dessus'] },
+            { title: 'Etape 2:', data: ['-Faire chauffer la bière dans une casserole'] },
+            { title: 'Etape 3:', data: ['-Ajouter les cheddar au fur et à mesure et mélanger sans cesse. Ajouter la moutarde et le poivre'] },
+            { title: 'Etape 4:', data: ['-Mélanger jusqua obtenir un texture homogène'] },
+            { title: 'Etape 5:', data: ['-Faire couler le cheddar fondu sur chaque tranche de pain'] },
+            { title: 'Etape 6:', data: ['-Enfourner 20 minutes à 180 degrès en chaleur tournante et déguster !'] },
+
           ]}
           renderItem={({ item }) => <Text style={style.sectionText}>{item}</Text>}
           renderSectionHeader={
-              ({ section }) => <Text style={style.sectionTitle}>{section.title}</Text>
+              ({ section }) => <Text style={style.sectionStep}>{section.title}</Text>
             }
           keyExtractor={(item, index) => index}
         />
