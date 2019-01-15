e8fabaa95fdab8a9bd4aefbe650ebd7efb01ee94
Thomas Fossaert <thomas.fossaert@epitech.eu>
2019-01-15 19:00:46 +0100

    [FEAT][RecipeApiCall] Adding some change for style, managing missing image, fixing recipe description

--- app/components/Recipe/RecipeDescriptionItem.js
+++ app/components/Recipe/RecipeDescriptionItem.js
@@ -15,10 +15,10 @@ const RecipeDescriptionItem = ({
       {recipe.title}
     </Text>
     <Image
-      source={{ uri: recipe.image_url }}
+      source={{ uri: recipe.image_url ? recipe.image_url : 'https://pngimage.net/wp-content/uploads/2018/06/not-found-png-3.png' }}
       style={style.imageStyle}
     />
-    <View style={{ flexDirection: 'row' }}>
+    <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
       <View style={style.infoContainer}>
         <Text style={style.indicator}>
           Temps:
