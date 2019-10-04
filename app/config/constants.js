a2323a2ce547b8a171a9ebe9603cb3121380e120
Guillaume CAUCHOIS <guillaume.cauchois@epitech.eu>
2019-10-04 15:46:22 +0200

    resolve #53

--- app/config/constants.js
+++ app/config/constants.js
@@ -1,2 +1,1 @@
-export const userDefaultProfileImage = 'https://espace-autoentrepreneur.com/img/default_profile.png';
-export const recipeDefaultImage = 'http://bottomlessbites.com/assets/recipe-placeholder.jpg';
+export const userDefaultProfileImage = 'https://www.zupimages.net/up/19/40/20mb.jpg';
