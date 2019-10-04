4f2a6d85da06b6226346ee7a9206a834277a643f
Guillaume CAUCHOIS <guillaume.cauchois@epitech.eu>
2019-10-04 16:22:03 +0200

    resolve #53 change to white bg

--- app/config/constants.js
+++ app/config/constants.js
@@ -1,2 +1,2 @@
-export const userDefaultProfileImage = 'https://www.zupimages.net/up/19/40/20mb.jpg';
+export const userDefaultProfileImage = 'https://zupimages.net/up/19/40/t37j.jpg';
 export const recipeDefaultImage = 'http://bottomlessbites.com/assets/recipe-placeholder.jpg';
