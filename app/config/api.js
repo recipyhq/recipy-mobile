19666074c588e8490c27d38cdc3c3520318fa2cd
Guillaume CAUCHOIS <guillaume.cauchois@epitech.eu>
2019-12-04 10:16:35 +0100

    force staging apk build

--- app/config/api.js
+++ app/config/api.js
@@ -1,3 +1,3 @@
-const ApiUrl = (process.env.NODE_ENV === 'development') ? 'https://staging-recipy.herokuapp.com' : 'http://recipy.fr';
+const ApiUrl = (process.env.NODE_ENV === 'development') ? 'https://staging-recipy.herokuapp.com' : 'https://staging-recipy.herokuapp.com';
 
 export default ApiUrl;
