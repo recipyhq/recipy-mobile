5b7a5c548aa32048e40a54fd3de4f782e93f8994
Guillaume CAUCHOIS <guillaume.cauchois@epitech.eu>
2019-12-05 19:35:17 +0100

    Prod linked

--- app/config/api.js
+++ app/config/api.js
@@ -1,3 +1,3 @@
-const ApiUrl = (process.env.NODE_ENV === 'development') ? 'https://staging-recipy.herokuapp.com' : 'https://staging-recipy.herokuapp.com';
+const ApiUrl = (process.env.NODE_ENV === 'development') ? 'https://staging-recipy.herokuapp.com' : 'https://recipy.fr/';
 
 export default ApiUrl;
